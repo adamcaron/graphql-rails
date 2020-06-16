20f0a4916df524297c068f42528b2d7c0c21299e
Adam Caron <adamwcaron@gmail.com>
2020-06-16 07:11:39 -0400

    relate links to votes
    link has many votes
    add votes as a field on the linkType

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -43,3 +43,46 @@ end
 # => #<Link id: 1, url: "http://graphql.org/", description: "The Best Query Language", created_at: "2020-06-16 01:46:51", updated_at: "2020-06-16 01:46:51">
 # 2.6.5 :002 > Link.create url: 'http://dev.apollodata.com/', description: 'Awesome GraphQL Client'
 # => #<Link id: 2, url: "http://dev.apollodata.com/", description: "Awesome GraphQL Client", created_at: "2020-06-16 01:47:48", updated_at: "2020-06-16 01:47:48">
+
+
+# And now...
+# after adding
+# votes to linkType
+# and has_many votes to Link
+# ... this
+# query {
+#   allLinks {
+#     votes {
+#       id
+#     }
+#   }
+# }
+# Returns this...
+# {
+#   "data": {
+#     "allLinks": [
+#       {
+#         "votes": []
+#       },
+#       {
+#         "votes": []
+#       },
+#       {
+#         "votes": []
+#       },
+#       {
+#         "votes": [
+#           {
+#             "id": "1"
+#           }
+#         ]
+#       },
+#       {
+#         "votes": []
+#       },
+#       {
+#         "votes": []
+#       }
+#     ]
+#   }
+# }
