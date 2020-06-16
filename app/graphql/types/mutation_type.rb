5d234d93d5d1b9ab3d7566d0a2c25adca53a3925
Adam Caron <adamwcaron@gmail.com>
2020-06-16 07:03:55 -0400

    create votes
    generate a vote model with user and link references
    vote belongs to each of them
    add a vote type, with id, UserType and LinkType fields
    define createVote resolver and add it the the mutations list

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,6 +3,7 @@ module Types
     # expose the create_link mutation
     field :create_user, mutation: Mutations::CreateUser
     field :create_link, mutation: Mutations::CreateLink
+    field :create_vote, mutation: Mutations::CreateVote
     field :signin_user, mutation: Mutations::SignInUser
   end
 end
